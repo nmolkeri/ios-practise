dbc49a2f1fb9f7ed00c0f0c9b89da47e921fa52e
Niranjan Molkeri <nmolkeri@gmail.com>
2017-02-04 22:45:57 -0800

    First commit of teh application

---
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Niranjan on 2/3/17.
+//  Copyright © 2017 Niranjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

e3634a94633b35ee7833356db987fadf52b9a255
Niranjan Molkeri <nmolkeri@gmail.com>
2017-02-07 02:09:57 -0800

    minor update to app

---
+++ stackUIApp/stackUIApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackUIApp
+//
+//  Created by Niranjan on 2/6/17.
+//  Copyright © 2017 Niranjan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
